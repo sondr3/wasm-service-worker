80232c9267a0a27b9470289c6e9402e6143f87c3
Sondre Aasemoen <sondre@eons.io>
2025-11-08 20:23:55 +0100

    Move some logic into the sw.ts file from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -12,20 +12,14 @@ pub fn start() -> std::result::Result<(), JsValue> {
         // we're in a service worker, so we can cast the global to a ServiceWorkerGlobalScope
         let global = global.unchecked_into::<ServiceWorkerGlobalScope>();
 
-        // Force immediate activation
-        let on_install = on_install(&global)?;
-        let on_activate = on_activate(&global)?;
-        global.set_oninstall(Some(on_install.as_ref().unchecked_ref()));
-        global.set_onactivate(Some(on_activate.as_ref().unchecked_ref()));
+        // Note: install/activate handlers are registered in sw.ts to avoid async timing issues
+        // They must be registered synchronously during initial script evaluation
 
-        // register all the other callbacks
+        // register message callback
         let on_message = on_message(&global)?;
         global.set_onmessage(Some(on_message.as_ref().unchecked_ref()));
 
-        // Ensure that the closures are not dropped before the service worker is terminated
-        // This is technically a memory leak, but I'm not sure that it matters in this case
-        on_install.forget();
-        on_activate.forget();
+        // Ensure that the closure is not dropped before the service worker is terminated
         on_message.forget();
     } else {
         console::log_1(&JsValue::from_str("not in service worker"));
@@ -33,28 +27,6 @@ pub fn start() -> std::result::Result<(), JsValue> {
     }
 
     Ok(())
-}
-
-fn on_install(
-    global: &ServiceWorkerGlobalScope,
-) -> std::result::Result<Closure<dyn FnMut(web_sys::ExtendableEvent)>, JsValue> {
-    let skip_waiting = global.skip_waiting()?;
-    Ok(Closure::wrap(
-        Box::new(move |event: web_sys::ExtendableEvent| {
-            event.wait_until(&skip_waiting).unwrap();
-        }) as Box<dyn FnMut(_)>,
-    ))
-}
-
-fn on_activate(
-    global: &ServiceWorkerGlobalScope,
-) -> std::result::Result<Closure<dyn FnMut(web_sys::ExtendableEvent)>, JsValue> {
-    let clients = global.clients();
-    Ok(Closure::wrap(
-        Box::new(move |event: web_sys::ExtendableEvent| {
-            event.wait_until(&clients.claim()).unwrap();
-        }) as Box<dyn FnMut(_)>,
-    ))
 }
 
 /// Displays a message in the console when a message is received from the client
--- src/sw.ts
+++ src/sw.ts
@@ -3,6 +3,19 @@ import init from "./pkg/wasm_service_worker.js"
 
 console.log("Service worker script loaded, initializing WASM...")
 
+// Event handlers MUST be registered synchronously during initial script evaluation
+self.addEventListener("install", (event) => {
+	console.log("Service worker installing...")
+	// @ts-ignore
+	self.skipWaiting()
+})
+
+self.addEventListener("activate", (event) => {
+	console.log("Service worker activating...")
+	// @ts-ignore
+	event.waitUntil(self.clients.claim())
+})
+
 // Initialize the WASM module
 // @ts-ignore
 init()
